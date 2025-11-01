72620424356326c5bca292eb4d814b0c1c5a74e1
umairansari92 <umair.ansari.92@gmail.com>
2025-11-02 03:20:24 +0500

    modified:   app.js

--- app.js
+++ app.js
@@ -10,7 +10,12 @@ dotenv.config();
 db();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://notemate.vercel.app"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
